cdff9b58031b8efae8591ca0fea6f8bbd4998d6e
avkatlant <avkatlant@gmail.com>
2022-02-06 18:23:09 +0700

    63 - React JS - практика, DAL, axios.create

---
+++ src/api/api.js
@@ -0,0 +1,17 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  headers: {
+    "API-KEY": "6b917dfc-dc0b-4ac2-a6d4-8e68d81f5ae5",
+  },
+});
+
+export const uesersAPI = {
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance
+      .get(`users/?page=${currentPage}&count=${pageSize}`)
+      .then((response) => response.data);
+  },
+};
