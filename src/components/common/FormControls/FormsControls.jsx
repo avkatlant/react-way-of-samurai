5d7fbff5f0a541428c013ff1d2668b7a68a7a737
avkatlant <avkatlant@gmail.com>
2022-02-12 14:42:57 +0700

    refactoring

--- src/components/common/FormControls/FormsControls.jsx
+++ src/components/common/FormControls/FormsControls.jsx
@@ -29,29 +29,3 @@ export const Input = (props) => {
     </FormControl>
   );
 };
-
-// export const Textarea = ({ input, meta, ...props }) => {
-//   const hasError = meta.touched && meta.error;
-
-//   return (
-//     <div className={styles.formControl + " " + (hasError ? styles.error : "")}>
-//       <div>
-//         <textarea {...input} {...props}></textarea>
-//       </div>
-//       {hasError && <span>{meta.error}</span>}
-//     </div>
-//   );
-// };
-
-// export const Input = ({ input, meta, ...props }) => {
-//   const hasError = meta.touched && meta.error;
-
-//   return (
-//     <div className={styles.formControl + " " + (hasError ? styles.error : "")}>
-//       <div>
-//         <input {...input} {...props}></input>
-//       </div>
-//       {hasError && <span>{meta.error}</span>}
-//     </div>
-//   );
-// };
